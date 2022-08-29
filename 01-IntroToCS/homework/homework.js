e92a26d2115725d6b3e85bc729a2091ecc80f9a0
PabloCasas129 <pablocasas1601@gmail.com>
2022-08-29 18:23:07 -0300

    vamos bien

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,28 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+  let resul = 0;
+  for(let i = 0; i < num.length; i++){
+    resul += 2**i * num[num.length-1-i];
+  }
+  return resul;
+
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+  let arr = [];
+  while(num != 0){
+    arr.unshift(num%2);
+    num = Math.floor(num/2)
+  }
+  return arr.join("");
+
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
